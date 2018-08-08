752d28a5f9a7f304c1da0d1a8615454c5c8e67ca
jtkim <jtkim@kaist.ac.kr>
2018-08-08 21:43:55 +0900

    updatee

---
+++ epd.py
@@ -0,0 +1,160 @@
+import sys
+sys.path.insert(0, './lib')
+
+import vad_func
+import os
+import matplotlib.pyplot as plt
+import librosa
+import numpy as np
+import time
+import tensorflow as tf
+from lib import config
+import glob
+import soundfile as sf
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+class EPD(object):
+
+    def __init__(self, win_size):
+
+        self.fs = int(16000)
+        self.speech_seg = win_size  # 100 ms
+        self.speech_minlen = int(self.fs * 0.001 * 2000)  # 1000 ms
+        self.speech_maxlen = int(self.fs * 0.001 * 10000)  # 5000 ms
+
+        self.speech_threshold = 0.5
+
+        self.transition_buffer_size = 5
+        self.transition_buffer = self.transition_buffer_size
+
+        self.data_queue_size = 2
+        self.speech_indx = 0
+        self.speech_state = 0
+
+        self.speech_data = []
+        self.data_queue = []
+        self.speech_queue = []
+
+        self.data_num = 0
+
+    def epd(self, data_seg):
+
+        input_speech_buffer = data_seg
+
+        # save data into the queue
+
+        if len(self.data_queue) < self.data_queue_size:
+            self.data_queue.append(input_speech_buffer)
+        elif len(self.data_queue) == self.data_queue_size:
+            self.data_queue.pop(0)
+            self.data_queue.append(input_speech_buffer)
+        else:
+            assert False, "queue size error"
+
+        # conduct vad
+
+        vad_result = vad_func.vad(input_speech_buffer)
+
+        if np.mean(vad_result) >= self.speech_threshold and self.speech_state == 0:
+            # start speech detection
+            self.speech_state = 1
+            self.speech_data.append(self.speech_indx)
+            self.speech_data.append(self.speech_indx + self.speech_seg)
+            self.speech_queue.append(np.concatenate(self.data_queue, axis=0))
+
+        elif np.mean(vad_result) >= self.speech_threshold and self.speech_state == 1:
+            # hold the speech detection
+            self.transition_buffer = self.transition_buffer_size
+            self.speech_state = 1
+            self.speech_data.append(self.speech_indx)
+            self.speech_data.append(self.speech_indx + self.speech_seg)
+            self.speech_queue.append(input_speech_buffer)
+
+        elif np.mean(vad_result) < self.speech_threshold and self.speech_state == 0:
+            # do not start speech detection
+            self.speech_state = 0
+            self.speech_queue = []
+
+        elif np.mean(vad_result) < self.speech_threshold and self.speech_state == 1:
+            if self.transition_buffer > 0:
+                # wait which the speech detection is really ended or not
+                self.transition_buffer = self.transition_buffer - 1
+                self.speech_data.append(self.speech_indx)
+                self.speech_data.append(self.speech_indx + self.speech_seg)
+                self.speech_queue.append(input_speech_buffer)
+            else:
+                self.speech_data = np.asarray(self.speech_data)
+                if self.speech_data[-1] - self.speech_data[0] >= self.speech_minlen:
+                    # if the length of detected speech is more than minimum length of speech, save the speech
+                    speech_final = np.concatenate(self.speech_queue, axis=0)
+
+                    print("save speech %d" % self.data_num)
+                    librosa.output.write_wav(
+                        './samples/epd_' + str(self.data_num).zfill(5) + '.wav', speech_final, self.fs)
+                    self.data_num = self.data_num + 1
+
+                    self.initialize()
+
+                elif self.speech_data[-1] - self.speech_data[0] >= self.speech_maxlen:
+                    speech_final = np.concatenate(self.speech_queue, axis=0)
+                    print("exceed the maximum speech length")
+                    print("save speech %d" % self.data_num)
+                    librosa.output.write_wav(
+                        './samples/epd_' + str(self.data_num).zfill(5) + '.wav', speech_final, self.fs)
+                    self.data_num = self.data_num + 1
+                    self.initialize()
+
+                else:
+                    # if the length of detected speech is less than minimum length of speech, holding the EPD
+                    # print(speech_data.shape[0])
+                    self.speech_state = 0
+                    self.transition_buffer = self.transition_buffer_size
+                    self.speech_data = []
+                    self.speech_queue = []
+
+        self.speech_indx = self.speech_indx + self.speech_seg
+
+    def initialize(self):
+
+        self.transition_buffer_size = 5
+        self.transition_buffer = self.transition_buffer_size
+
+        self.speech_indx = 0
+        self.speech_state = 0
+
+        self.speech_data = []
+        self.data_queue = []
+        self.speech_queue = []
+
+
+if __name__ == '__main__':
+
+    prj_dir = os.path.dirname(__file__)
+
+    clean_list = sorted(glob.glob(prj_dir + '/*.wav'))
+
+    win_size = int(config.fs * 0.001 * 200)  # 200 ms
+
+    data, sr = sf.read(clean_list[0])
+    data = librosa.core.resample(data, sr, config.fs)
+    data = np.concatenate([data, np.squeeze(np.random.random([30000, 1]) * 0.0001)], axis=0)
+    print("total speech time: %.4f" % (data.shape[0]/config.fs))
+
+    Detector = EPD(win_size)
+
+    start_idx = 0
+    start_time = time.time()
+
+    while True:
+
+        if start_idx + win_size > data.shape[0]:
+            break
+        data_stream = data[start_idx:start_idx+win_size]
+        Detector.epd(data_stream)
+        start_idx = start_idx + win_size
+
+    end_time = time.time() - start_time
+    print(end_time)
---
+++ lib/vad_func.py
@@ -0,0 +1,87 @@
+import os
+from lib import graph_test as gt
+from lib import utils
+from lib import config
+from sklearn.feature_extraction import image
+import scipy.io, scipy.io.wavfile, scipy.signal
+import librosa
+import glob
+import tensorflow as tf
+import numpy as np
+import time
+
+# prj_dir = os.path.dirname('..')
+norm_dir = os.path.abspath('./data/train/norm'+'/norm.mat')
+graph_name = sorted(glob.glob(os.path.abspath('./saved_model/*.pb')))[-1]
+graph = gt.load_graph(graph_name)
+sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+sess = tf.Session(graph=graph, config=sess_config)
+
+norm_param = scipy.io.loadmat(norm_dir)
+
+mag_mean = norm_param["mag_mean"]
+mag_std = norm_param["mag_std"]
+
+train_mean = np.transpose(mag_mean, (1, 0))
+train_std = np.transpose(mag_std, (1, 0))
+
+speech_pad = np.zeros(int(config.fs * 0.001 * 50))
+
+
+def get_feature(data):
+
+    result = get_mfsc(data)
+
+    result = (result - train_mean) / train_std
+    pad = np.expand_dims(np.zeros((int(config.time_width / 2), result.shape[1])), axis=2)
+
+    result = np.squeeze(np.concatenate((pad, result, pad), axis=0))
+
+    result = image.extract_patches_2d(result, (config.time_width, config.n_mels))
+
+    result = np.expand_dims(result, axis=3)
+    return result
+
+
+def get_mfsc(data):
+
+    data = np.asarray(data).astype(dtype=np.float32)
+
+    Zxx = librosa.feature.melspectrogram(data, sr=config.fs, n_fft=int(config.nfft),
+                                         hop_length=int(config.nperseg * 0.5),
+                                         n_mels=config.n_mels, fmin=300, fmax=8000)
+
+    mfsc = np.transpose(np.expand_dims(Zxx, axis=2), (1, 0, 2))[:-1, :]
+
+    return mfsc
+
+
+def vad(speech):
+
+    speech = np.concatenate([speech_pad, speech, speech_pad], axis=0)
+
+    node_inputs = graph.get_tensor_by_name('prefix/model_1/inputs:0')
+    node_labels = graph.get_tensor_by_name('prefix/model_1/labels:0')
+    node_softpred = graph.get_tensor_by_name('prefix/model_1/softpred:0')
+
+    inputs = get_feature(speech)
+    fake_outputs = np.zeros(inputs.shape[0])
+    feed_dict = {node_inputs: inputs, node_labels: fake_outputs}
+    start_time = time.time()
+
+    # sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    # # sess_config.gpu_options.allow_growth = True
+    # with tf.Session(graph=graph, config=sess_config) as sess:
+    softpred = sess.run(node_softpred, feed_dict=feed_dict)
+
+    softpred = softpred[5:-5]
+    pred = np.int32(softpred > 0.5)
+    raw_pred = utils.frame2rawlabel(pred, config.nperseg, int(config.nperseg * 0.5))[0:speech.shape[0]]
+
+    # print(time.time() - start_time)
+
+    return raw_pred
+
+
+if __name__ == '__main__':
+    pass
